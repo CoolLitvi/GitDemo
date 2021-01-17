3ce55e949fa9f4e8a5c4de3c7cb6e726df34b39d
vova <v.o.litvinchuk@gmail.com>
2021-01-18 00:54:47 +0200

    my first commit

---
+++ src/test/java/qaclickacademy/MavenJava/RestApiTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+
+	}
+
+}
